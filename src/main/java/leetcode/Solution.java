ae12b35782203baf97f02873684135b3e9a1f4e6
Lucious Morrison <lmorrison1116@gmail.com>
2020-07-12 20:02:06 -0400

    created happyNumber

---
+++ src/main/java/leetcode/Solution.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Solution {
+
+
+    public boolean isHappy(int n) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        temp = n;
+        
+    }
+}
